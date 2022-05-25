1bc9897f0bca40ab1d6595e35e5a41b5d8ee684a
satyam147 <satyam161211149@gmail.com>
2022-05-25 12:05:42 +0530

    Authentication and Authorization

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../dbconnection');
+var jwt = require('jsonwebtoken');
 
 router.post('/login', async (req, res) => {
     let body = req.body;
-    console.log(body);
     let sql = `SELECT *
                FROM users
                WHERE email = '${body.email}'
-                 AND password = '${body.password}' limit 1`;
+                 AND password = '${body.password}'
+               limit 1`;
     let users = await db.promise().query(sql);
     let user = users[0];
-    console.log(user);
     if (user.length == 0) {
         res.status(422).send('Email or Password Invalid')
     } else {
+        let token = jwt.sign({id: user[0].id}, 'hsgdjhfsdagsfdgfdas');
+        user[0]['token'] = token;
         res.status(200).send(user[0]);
     }
 });
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../dbconnection');
+const jwt = require('jsonwebtoken');
 
-router.post('/create', (req, res) => {
+var user_id = '';
+
+router.post('/create',checkUser,restrictedUser, (req, res) => {
     let body = req.body;
     let sql = `INSERT INTO users (name, email)
                values ('${body.name}', '${body.email}')`;
@@ -14,7 +17,7 @@ router.post('/create', (req, res) => {
     res.status(200).send(['user created']);
 });
 
-router.post('/list', async (req, res) => {
+router.post('/list', checkUser, restrictedUser, async (req, res) => {
     let users = await db.promise().query('SELECT * FROM users');
     res.status(200).send(users[0]);
 });
@@ -29,14 +32,43 @@ router.post('/view', async (req, res) => {
 
 router.post('/update', (req, res) => {
     let body = req.body;
-    let update = db.promise().query(`UPDATE users SET name  = '${body.name}',email = '${body.email}' WHERE id = '${body.id}'`);
+    let update = db.promise().query(`UPDATE users
+                                     SET name  = '${body.name}',
+                                         email = '${body.email}'
+                                     WHERE id = '${body.id}'`);
     res.status(200).send(['Updated']);
 });
 
-router.post('/delete',(req,res)=>{
+router.post('/delete', (req, res) => {
     let body = req.body;
-    let remove = db.promise().query(`DELETE FROM users WHERE id = '${body.id}'`);
+    let remove = db.promise().query(`DELETE
+                                     FROM users
+                                     WHERE id = '${body.id}'`);
     res.status(200).send(['deleted']);
 })
+
+
+function checkUser(req, res, next) {
+    let query = req.query;
+    let token = query.token;
+    if (!token) {
+        res.status(401).send(['Unauthorized'])
+    }
+    let data = jwt.verify(token, 'hsgdjhfsdagsfdgfdas');
+    if (!data.id) {
+        res.status(422).send('Invalid Token');
+    }
+    user_id = data.id;
+    next();
+}
+
+function restrictedUser(req, res, next) {
+    let restricted_ids = [1, 2, 5];
+    if (restricted_ids.includes(user_id)) {
+        res.status(401).send('User Restricted');
+    }
+    next();
+}
+
 
 module.exports = router;
